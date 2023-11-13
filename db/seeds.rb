77d25cc707b0d2abfa3746ff3992b4d8aba3d035
Maxime Oudin <oudinmaxime@gmail.com>
2023-11-13 12:07:03 +0100

    seed  with 5 restaurants done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Review.all.destroy
+# puts 'Review destroyed'
+
+Restaurant.all.destroy
+puts 'Restaurant destroyed'
+
+bristol = Restaurant.new(name: "Epicure", category: "french")
+bristol.address = "75008 Paris"
+bristol.save
+
+pistol = Restaurant.new(name: "Pistolero", category: "italian")
+pistol.address = "75002 Paris"
+pistol.save
+
+madoname = Restaurant.new(name: "Madona'Me", category: "italian")
+madoname.address = "33000 Bordeaux"
+madoname.save
+
+sushi = Restaurant.new(name: "Sushi Shop", category: "japanese")
+sushi.address = "33100 Begles"
+sushi.save
+
+leon = Restaurant.new(name: "Leon de Bruxelles", category: "belgian")
+leon.address = "33600 Villenave"
+leon.save
+
+puts 'Restaurant creations'
+# puts 'Review creations'
